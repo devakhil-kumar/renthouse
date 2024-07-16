fb2c3aac73d385d75fb1296fee7775cafdcb38d1
devakhil-kumar <akhil.cyberbells@gmail.com>
2024-07-16 10:56:22 +0530

    sdf

--- App.jsx
+++ App.jsx
@@ -20,6 +20,8 @@ const AppWrapper = () => {
       }
     };
 
+
+    
     init().finally(async () => {
       await BootSplash.hide({ fade: true });
       console.log("BootSplash has been hidden successfully");
